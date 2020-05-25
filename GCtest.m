7b17452675fb57ecdf261693e2d08dea2c8e411e
Erin <65602256+Soyeon-ErinLee@users.noreply.github.com>
2020-05-25 16:45:46 +0900

    Update GCtest.m

--- GCtest.m
+++ GCtest.m
@@ -3,16 +3,6 @@ function [Wald,pval]=GCtest(y1,y2,p)
 % Wald is the value of the test stat
 % pval is the p-value
 % B'==[v,A1,...,Ap]
-% v가 k x 1, A들이 k x k이기에 B'는 k x 1+kp matrix.
-% once you vectorize B, B'의 each row가 하나의 column 아래 착착 쌓임(stack).
-% 즉 K*(1+k*p) x 1 "by one vector"가 되는 것.
-% Cmat shows the position which coefficients of are tested for zero in B' one by one
-% 선형모형방법론에서 beta에 곱해지는 Cmat와 비슷한 역할인 듯.
-% kxk matrix인 A 쪼개서
-% | k1xk1   k1xk2 |  <- 0으로 만들고 싶은 부분
-% | k2xk1   k2xk2 |
-% s번째 row, A(r)에 있는 애 test하려면 
-% (s-1)(1+K*p) + (r-1)*K+1 + k1 + q(반복) 번째 애를 를 1로 만들고 test
 
 k1=size(y1,2);  
 k2=size(y2,2);
@@ -41,8 +31,8 @@ disp('H0: y2 does not Granger cause y1')
 disp('')
 disp(['The p-value is' num2str(pval)])
 disp('')
-pos=repmat('*',K,1+K*p); %처음엔 *로 이루어진 matrix 만들기
-pos((Cmat==1))=['0']; % Cmat==1인 곳을 0으로 바꿈
+pos=repmat('*',K,1+K*p); 
+pos((Cmat==1))=['0'];
 disp(['Imposed zero restrictions are of the form'])
 disp(['[v,A1,...,Ap] = '])
 disp(pos)
